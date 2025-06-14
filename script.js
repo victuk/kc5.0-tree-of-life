3506ef3fd705182a32ae80224e02b7e1eed7335a
Victor Ukok <ukokjnr@gmail.com>
2025-06-14 14:35:30 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const users = ["Jesam", "Matilda", "Kingsley", "Emmanuel", "Johnson", "Abdulatif"];
+
+// for(start; stop; step) {
+
+// }
+
+for(let i = 0; i < 5; i++) {
+    console.log("Hello", i);
+}
+
